eabd7698a8700c8202cd345af67763f5dd0f3d62
rahat <rsdrahat@gmail.com>
2022-03-13 14:47:20 -0400

    Updates

---
+++ precache-manifest.a66914a24bd5164d3569c6539a9737d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8e8f0abd657d52192f0",
+    "url": "/profile/static/css/main.a5a591c9.chunk.css"
+  },
+  {
+    "revision": "e8e8f0abd657d52192f0",
+    "url": "/profile/static/js/main.db9ae2d1.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/profile/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "b7f1f97a2d99e7766a8f6b307b0ddb4f",
+    "url": "/profile/index.html"
+  }
+];
