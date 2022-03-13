faf709c0b05f335a96d7ef2faac7b3928b8a0984
rahat <rsdrahat@gmail.com>
2022-03-13 14:44:00 -0400

    Updates

---
+++ precache-manifest.2801ea76c8f0f90788deb288002c1469.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be54b8b65e7df1a15c4f",
+    "url": "/my-landing-page/static/css/main.a5a591c9.chunk.css"
+  },
+  {
+    "revision": "be54b8b65e7df1a15c4f",
+    "url": "/my-landing-page/static/js/main.d7d3ebcf.chunk.js"
+  },
+  {
+    "revision": "ee3051d1287202236446",
+    "url": "/my-landing-page/static/js/runtime~main.58193f6c.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/my-landing-page/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "3dcbe76ba72ba03b1845e892a8a486c6",
+    "url": "/my-landing-page/index.html"
+  }
+];
